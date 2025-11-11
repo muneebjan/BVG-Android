b76984a9779f56afc74a67588529c67d558517d7
muneebjan <muneebahsan07@gmail.com>
2025-11-11 21:40:02 +0100

    fix: refine model

--- app/src/main/java/com/example/bvgnativ/data/model/Departure.kt
+++ app/src/main/java/com/example/bvgnativ/data/model/Departure.kt
@@ -1,7 +1,24 @@
 package com.example.bvgnativ.data.model
 
+import com.google.gson.annotations.SerializedName
+
+data class DeparturesResponse(
+    val departures: List<Departure>,
+    val realtimeDataUpdatedAt: Long
+)
+
 data class Departure(
-    val line: String,
-    val direction: String,
-    val plannedWhen: String
+    val tripId: String?,
+    val direction: String?,
+    val line: Line?,
+    @SerializedName("when") val whenTime: String?, // 'when' is a reserved word
+    val plannedWhen: String?,
+    val delay: Int?
+)
+
+data class Line(
+    val id: String?,
+    val name: String?,
+    val mode: String?,
+    val product: String?
 )
--- app/src/main/java/com/example/bvgnativ/data/model/Stop.kt
+++ app/src/main/java/com/example/bvgnativ/data/model/Stop.kt
@@ -1,8 +1,23 @@
 package com.example.bvgnativ.data.model
 
 data class Stop(
-    val id: String,
-    val name: String,
-    val latitude: Double,
-    val longitude: Double
+    val type: String?,
+    val id: String?,
+    val name: String?,
+    val location: Location?,
+    val products: Products?
+)
+
+data class Location(
+    val type: String?,
+    val id: String?,
+    val latitude: Double?,
+    val longitude: Double?
+)
+
+data class Products(
+    val suburban: Boolean? = false,
+    val subway: Boolean? = false,
+    val tram: Boolean? = false,
+    val bus: Boolean? = false
 )
